f260fef824dcc040cf20d05cc53f2619f8e66b32
DarkPhisyc <darkphisyc50@gmail.com>
2026-03-01 19:41:15 -0600

    Creación de la Ventana

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
 public class Test {
-    
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana(400, 200, 700, 500, "Simulación");
+    }
 }
--- Ventana.java
+++ Ventana.java
@@ -1,3 +1,12 @@
-public class Ventana {
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
     
+    public Ventana(int x, int y, int ancho, int alto, String titulo) {
+        super(titulo);
+        super.setBounds(x, y, ancho, alto);
+        super.setLayout(null);
+        super.setVisible(true);
+        super.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
