d02dc5e65ee4d56c40b23b298f139be09412bbe2
Msol9 <ms.bksol@gmail.com>
2026-03-03 12:24:49 -0600

    Ingresar datos m3

--- Gestion.java
+++ Gestion.java
@@ -1,7 +1,10 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
+
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class Gestion implements ActionListener {
     @Override
@@ -12,6 +15,21 @@ public class Gestion implements ActionListener {
             case "2":
                 break;
             case "3":
+                Scanner lector = new Scanner(System.in);
+                double semillaX, constante;
+                byte D, n;
+                System.out.println("Algoritmo de multiplicador constante");
+
+                System.out.println("Ingresa el valor de la semilla Xi: ");
+                semillaX=lector.nextDouble();
+                System.out.println("Ingresa el valor de la constante: ");
+                constante=lector.nextDouble();
+                System.out.println("Ingresa la cantidad de numeros pseudoaleatorios que necesitas:");
+                n=lector.nextByte();
+
+                for (int i = 0; i <=n; i++) {
+                    double y= semillaX * constante;
+                }
                 break;
             case "4":
                 break;
