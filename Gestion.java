338bef6be675688811662b31e3d9276d60b3380c
DarkPhisyc <darkphisyc50@gmail.com>
2026-03-03 11:30:20 -0600

    Switch en Gestion

--- Gestion.java
+++ Gestion.java
@@ -1,3 +1,27 @@
-public class Gestion {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Gestion implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch(e.getActionCommand()) {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            case "6":
+                break;
+            case "7":
+                break;
+            default:
+                break;
+        }
+    }
     
 }
